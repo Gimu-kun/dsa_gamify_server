fc1ee69dafa66fc3946a806585c53163cce529c2
Gimu-kun <hieunghiemle@gmail.com>
2025-08-26 08:05:07 +0700

    feat: utils creation
    Firebase util (upload file, image)
    Jwt util (create token, decode token)
    passwords util (hash, check passwords)

---
+++ src/main/java/com/example/dsa_gamify_be/utils/PasswordsUtil.java
@@ -0,0 +1,18 @@
+package com.example.dsa_gamify_be.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordsUtil {
+    static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public static String hashPassword(String pw){
+        return passwordEncoder.encode(pw);
+    };
+
+    public static boolean checkPassword(String rawPw, String encodedPw){
+        return passwordEncoder.matches(rawPw,encodedPw);
+    };
+}
