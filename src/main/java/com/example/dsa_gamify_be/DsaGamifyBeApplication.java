d2e167d39cde25cb9882deacc8b0bf97403d5337
Gimu-kun <hieunghiemle@gmail.com>
2025-08-26 08:10:55 +0700

    init: project initial

---
+++ src/main/java/com/example/dsa_gamify_be/DsaGamifyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsa_gamify_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaGamifyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaGamifyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dsa_gamify_be/DsaGamifyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsa_gamify_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaGamifyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
